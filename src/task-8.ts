0b7cba6ef6a623a7dfa391b3398602ea9b732a9e
Andrii Hordiienko <30300588+drew875@users.noreply.github.com>
2025-06-06 21:53:50 +0300

    hw1

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,9 +1,17 @@
-function getMessage(): Promise<string> {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Hello from TS");
-        }, 1000);
+import axios from "axios";
 
-    });
+interface Post {
+    id: number;
+    title: string;
+    body: string;
 }
-getMessage().then(result => console.log(result))
+async function fetchPosts(): Promise<Post[]> {
+    const response = await axios.get<Post[]>(
+        '<https://jsonplaceholder.typicode.com/posts>'
+    );
+    return response.data;
+}
+
+fetchPosts().then((posts) => {
+    console.log(posts[0].title);
+});
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/01-ts-basics/', // 👈 обязательно укажи имя репозитория с косыми чертами
+});
